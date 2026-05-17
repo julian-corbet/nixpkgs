2bc59e7be101d0da11d8a7eebf06c3003af223e2
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-17 16:33:33 +0200

    python3Packages.tensorboard: pin to setuptools_81

--- pkgs/development/python-modules/tensorboard/default.nix
+++ pkgs/development/python-modules/tensorboard/default.nix
@@ -11,7 +11,7 @@
   packaging,
   pillow,
   protobuf,
-  setuptools,
+  setuptools_81,
   tensorboard-data-server,
   werkzeug,
   standard-imghdr,
@@ -47,7 +47,7 @@ buildPythonPackage rec {
     packaging
     pillow
     protobuf
-    setuptools
+    setuptools_81
     tensorboard-data-server
     werkzeug
 
